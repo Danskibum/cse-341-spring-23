d0f12ebb1bb395a9d41ddc0501888ac8b1c7fe87
DANIEL GERDES <ger19007@byui.edu>
2023-04-22 22:31:30 -0700

    going above

---
+++ controllers/lesson1.js
@@ -0,0 +1,21 @@
+const kristalRoute = (req,res) => {
+    res.send("Kristal Gerdes");
+  };
+
+const gretchenRoute = (req,res) => {
+    res.send("Gretchen Gerdes");
+  };
+  
+const abigailRoute = (req,res) => {
+    res.send("Abigail Gerdes");
+  };
+
+const nathanRoute = (req,res) => {
+    res.send("Nathan Gerdes");
+  };
+  module.exports ={
+    kristalRoute,
+    gretchenRoute,
+    abigailRoute,
+    nathanRoute,
+  };
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const routes = require('express').Router();
+const lesson1Controller = require('../controllers/lesson1');
+
+routes.get('/', lesson1Controller.kristalRoute);
+routes.get('/Gretchen', lesson1Controller.gretchenRoute);
+routes.get('/Abigail', lesson1Controller.abigailRoute);
+routes.get('/Nathan', lesson1Controller.nathanRoute);
+
+module.exports = routes;
